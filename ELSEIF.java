8677fe87036c5070493098f4763d3fb61f3b1bcd
Eric Imanzi <125451546+imanzi123@users.noreply.github.com>
2023-11-03 10:37:37 +0200

    elseif loop

---
+++ ELSEIF.java
@@ -0,0 +1,29 @@
+public class ELSEIF {
+
+    public static void main(String[]arg)
+    { 
+        String food = "chicken";
+
+    if(food == "pork"){
+        System.out.println("food is delicious");
+    }
+    else if(food == "beef"){
+        System.out.println("food is expensive");
+    }
+    else if(food == "chicken"){
+        System.out.println("food is perfect choice");
+    
+    }
+    else if(food == "hen"){
+        System.out.println("food is cheap");
+    }
+    else {
+        System.out.println("food doesnt exist"); 
+    }
+
+
+    }
+
+    
+
+}
