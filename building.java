fa6950d33021d145e1561889daa4f54b99c66861
Eric Imanzi <125451546+imanzi123@users.noreply.github.com>
2023-11-08 14:11:46 +0200

    Inheritance classwork

--- building.java
+++ building.java
@@ -1,3 +1,4 @@
+
 interface BuildingMaterial {
     void describeMaterial();
 }
---
+++ trees.java
@@ -0,0 +1,40 @@
+
+interface Tree {
+    void grow();
+}
+
+
+class MapleTree implements Tree {
+    public void grow() {
+        System.out.println("A maple tree is growing.");
+    }
+
+    public void produceSeeds() {
+        System.out.println("A maple tree is producing seeds.");
+    }
+}
+
+
+class PalmTree implements Tree {
+    public void grow() {
+        System.out.println("A palm tree is growing.");
+    }
+
+    public void provideShade() {
+        System.out.println("A palm tree provides shade in hot climates.");
+    }
+}
+
+public class trees{
+    public static void main(String[] args) {
+        
+        Tree maple = new MapleTree();
+        maple.grow();
+        ((MapleTree) maple).produceSeeds();
+
+        // Create a palm tree object
+        Tree palm = new PalmTree();
+        palm.grow();
+        ((PalmTree) palm).provideShade();
+    }
+}
